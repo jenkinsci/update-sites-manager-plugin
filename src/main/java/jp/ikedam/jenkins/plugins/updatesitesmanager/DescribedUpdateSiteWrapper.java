d7b0716b41bd8dfc94d44fabc3bc196078bbe3d3
ikedam <devld@ikedam.jp>
2013-03-18 22:56:27 +0900

    Add comments, translate DescribedUpdateSiteWrapper.

--- src/main/java/jp/ikedam/jenkins/plugins/updatesitesmanager/DescribedUpdateSiteWrapper.java
+++ src/main/java/jp/ikedam/jenkins/plugins/updatesitesmanager/DescribedUpdateSiteWrapper.java
@@ -40,6 +40,12 @@ public class DescribedUpdateSiteWrapper extends DescribedUpdateSite
 {
     private static final long serialVersionUID = -381591686508537334L;
     
+    /**
+     * Constructor for new UpdateSite
+     * 
+     * @param id
+     * @param url
+     */
     @DataBoundConstructor
     public DescribedUpdateSiteWrapper(String id, String url)
     {
@@ -47,6 +53,11 @@ public class DescribedUpdateSiteWrapper extends DescribedUpdateSite
         this.updateSite = new UpdateSite(id, url);
     }
     
+    /**
+     * Constructor from existing UpdateSite
+     * 
+     * @param updateSite
+     */
     public DescribedUpdateSiteWrapper(UpdateSite updateSite)
     {
         super(null, null);
@@ -55,24 +66,52 @@ public class DescribedUpdateSiteWrapper extends DescribedUpdateSite
     
     private UpdateSite updateSite;
     
+    /**
+     * Returns UpdateSite to register to Jenkins.
+     * 
+     * @return UpdateSite
+     * @see jp.ikedam.jenkins.plugins.updatesitesmanager.DescribedUpdateSite#getUpdateSite()
+     */
     @Override
     public UpdateSite getUpdateSite()
     {
         return updateSite;
     }
     
+    /**
+     * Returns site id.
+     * 
+     * @return site id
+     * @see hudson.model.UpdateSite#getId()
+     */
     @Override
     public String getId()
     {
         return getUpdateSite().getId();
     }
     
+    /**
+     * Returns the URL of the site.
+     * 
+     * @return the URL of the site
+     * @see hudson.model.UpdateSite#getUrl()
+     */
     @Override
     public String getUrl()
     {
         return getUpdateSite().getUrl();
     }
     
+    /**
+     * Returns whether this UpdateSite is editable by UpdateSiteManager
+     * 
+     * Editable only when wrapped object is an instance of UpdateSite.
+     * Subclass of UpdateSite is not allowed, for there may be fields UpdateSiteManager does not know.
+     * The site whose id is "default" is also ineditable, for it must be managed in UpdateCenter.
+     * 
+     * @return whether this UpdateSite is editable
+     * @see jp.ikedam.jenkins.plugins.updatesitesmanager.DescribedUpdateSite#isEditable()
+     */
     @Override
     public boolean isEditable()
     {
@@ -80,29 +119,50 @@ public class DescribedUpdateSiteWrapper extends DescribedUpdateSite
                 && !"default".equals(getId());
     }
     
-    @Override
-    public Descriptor getDescriptor()
-    {
-        return DescriptorImpl.DESCRIPTOR;
-    }
-    
+    /**
+     * Descriptor for this class.
+     */
     @Extension
     static public class DescriptorImpl extends DescribedUpdateSite.Descriptor
     {
         static DescriptorImpl DESCRIPTOR = new DescriptorImpl();
         
+        /**
+         * Returns the displaying name. 
+         * 
+         * This is not used for this class, for canCreateNewSite returns false.
+         * 
+         * @return the displaying name
+         * @see hudson.model.Descriptor#getDisplayName()
+         */
         @Override
         public String getDisplayName()
         {
             return Messages.DescribedUpdateSiteWrapper_DisplayName();
         }
         
+        /**
+         * Returns the description of this class.
+         * 
+         * This is not used for this class, for canCreateNewSite returns false.
+         * 
+         * @return the description of this class
+         * @see jp.ikedam.jenkins.plugins.updatesitesmanager.DescribedUpdateSite.Descriptor#getDescription()
+         */
         @Override
         public String getDescription()
         {
             return Messages.DescribedUpdateSiteWrapper_Description();
         }
         
+        /**
+         * Returns whether this class can be used to create new UpdateSite.
+         * 
+         * Always returns false for this class is used only for managing existing UpdateSite. 
+         * 
+         * @return false
+         * @see jp.ikedam.jenkins.plugins.updatesitesmanager.DescribedUpdateSite.Descriptor#canCreateNewSite()
+         */
         @Override
         public boolean canCreateNewSite()
         {
