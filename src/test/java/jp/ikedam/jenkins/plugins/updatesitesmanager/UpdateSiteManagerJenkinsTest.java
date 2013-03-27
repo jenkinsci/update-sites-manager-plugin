6a1ce1dc74baa4da4c02a4006d50b5ef01969f66
ikedam <devld@ikedam.jp>
2013-03-27 23:37:33 +0900

    Completed test for UpdateSitesManager.

--- src/test/java/jp/ikedam/jenkins/plugins/updatesitesmanager/UpdateSiteManagerJenkinsTest.java
+++ src/test/java/jp/ikedam/jenkins/plugins/updatesitesmanager/UpdateSiteManagerJenkinsTest.java
@@ -12,11 +12,19 @@ import java.util.List;
 
 import jenkins.model.Jenkins;
 
+import net.sf.json.JSONObject;
+
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.TestExtension;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 import org.xml.sax.SAXException;
 
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.google.common.collect.Lists;
 
@@ -31,10 +39,6 @@ public class UpdateSiteManagerJenkinsTest extends HudsonTestCase
     public void testManagementLink() throws IOException, SAXException
     {
         WebClient wc = new WebClient();
-        
-        // make output quiet.
-        // comment out here if an unexpected behavior occurs.
-        wc.setPrintContentOnFailingStatusCode(false);
         
         HtmlPage managementPage = wc.goTo("/manage");
         assertNotNull(
@@ -51,11 +55,7 @@ public class UpdateSiteManagerJenkinsTest extends HudsonTestCase
         
         WebClient wc = new WebClient();
         
-        // make output quiet.
-        // comment out here if an unexpected behavior occurs.
-        wc.setPrintContentOnFailingStatusCode(false);
-        
-        HtmlPage updateSitesPage = wc.goTo(String.format("/%s", UpdateSitesManager.URL));
+        HtmlPage updateSitesPage = wc.goTo(UpdateSitesManager.URL);
         HtmlElement table = updateSitesPage.getElementById("update-sites");
         DomNodeList<HtmlElement> trs = table.getElementsByTagName("tr");
         assertEquals(
@@ -91,7 +91,7 @@ public class UpdateSiteManagerJenkinsTest extends HudsonTestCase
         // comment out here if an unexpected behavior occurs.
         wc.setPrintContentOnFailingStatusCode(false);
         
-        HtmlPage updateSitesPage = wc.goTo(String.format("/%s", UpdateSitesManager.URL));
+        HtmlPage updateSitesPage = wc.goTo(UpdateSitesManager.URL);
         HtmlElement table = updateSitesPage.getElementById("update-sites");
         DomNodeList<HtmlElement> trs = table.getElementsByTagName("tr");
         assertEquals(
@@ -216,15 +216,306 @@ public class UpdateSiteManagerJenkinsTest extends HudsonTestCase
             
             assertSame("should return specified site", site1, target.getDynamic("test1").getUpdateSite());
             assertSame("should return specified site", site2, target.getDynamic("test2").getUpdateSite());
-            assertNull("Null should be return for unknown site", target.getDynamic("nosite"));
-            assertNull("Null should be return for null", target.getDynamic(null));
-            assertNull("Null should be return for empty", target.getDynamic(""));
+            assertNull("Null should be returned for unknown site", target.getDynamic("nosite"));
+            assertNull("Null should be returned for null", target.getDynamic(null));
+            assertNull("Null should be returned for empty", target.getDynamic(""));
         }
     }
     
-    public void testDo_add()
+    public void testDo_add() throws IOException, SAXException
     {
-        // TODO
+        UpdateSitesManager target = new UpdateSitesManager();
+        assertEquals("This test must run with one UpdateSite Descriptors registered.", 1, target.getUpdateSiteDescriptorList().size());
+        
+        WebClient wc = new WebClient();
+        
+        HtmlPage updateSitesPage = wc.goTo(UpdateSitesManager.URL);
+        
+        // follow "Add New Site"
+        HtmlAnchor addNewSiteLink = (HtmlAnchor)updateSitesPage.getElementById("add-new-site-link");
+        HtmlPage addNewSitePage = wc.getPage(updateSitesPage.getFullyQualifiedUrl(addNewSiteLink.getHrefAttribute()));
+        
+        // Verify form
+        HtmlForm addNewSiteForm = addNewSitePage.getFormByName("addSiteForm");
+        assertNotNull("Form page is not shown", addNewSiteForm);
     }
     
+    public void testDo_addWithMultiUpdateSites() throws Exception
+    {
+        // initialize UpdateSites.
+        UpdateSite site1 = new UpdateSite(
+            "test1",
+            "http://example.com/test/update-center.json"
+        );
+        UpdateSite site2 = new ManagedUpdateSite(
+            "test2",
+            "http://example.com/test2/update-center.json",
+            false,
+            null,
+            null,
+            false
+        );
+        Jenkins.getInstance().getUpdateCenter().getSites().clear();
+        Jenkins.getInstance().getUpdateCenter().getSites().add(site1);
+        Jenkins.getInstance().getUpdateCenter().getSites().add(site2);
+        
+        UpdateSitesManager target = new UpdateSitesManager();
+        assertTrue("This test must run with more than one UpdateSite Descriptors registered.", 1 < target.getUpdateSiteDescriptorList().size());
+        
+        WebClient wc = new WebClient();
+        
+        HtmlPage updateSitesPage = wc.goTo(UpdateSitesManager.URL);
+        
+        // follow "Add New Site"
+        HtmlAnchor addNewSiteLink = (HtmlAnchor)updateSitesPage.getElementById("add-new-site-link");
+        HtmlPage addNewSitePage1 = wc.getPage(updateSitesPage.getFullyQualifiedUrl(addNewSiteLink.getHrefAttribute()));
+        
+        // Verify Form
+        HtmlForm selectNewSiteForm = addNewSitePage1.getFormByName("selectNewSiteForm");
+        assertNotNull("A page to select a site type is not shown", selectNewSiteForm);
+        
+        // Sending form without selecting a site results returning to the same page.
+        addNewSitePage1 = submit(selectNewSiteForm);
+        selectNewSiteForm = addNewSitePage1.getFormByName("selectNewSiteForm");
+        assertNotNull("Submit without selecting a site must results in returning to the same page.", selectNewSiteForm);
+        
+        // Sending form with selecting an invalid site results returning to the same page.
+        addNewSitePage1 = submit(selectNewSiteForm);
+        {
+            HtmlInput input = selectNewSiteForm.getInputByValue(Jenkins.getInstance().getDescriptor(TestUpdateSite.class).getClass().getName());
+            input.setValueAttribute("foobar");
+            input.setChecked(true);
+        }
+        selectNewSiteForm = addNewSitePage1.getFormByName("selectNewSiteForm");
+        assertNotNull("Submit with selecting a invalid value must results in returning to the same page.", selectNewSiteForm);
+        
+        selectNewSiteForm.getInputByValue(Jenkins.getInstance().getDescriptor(TestUpdateSite.class).getClass().getName()).setChecked(true);
+        HtmlPage addNewSitePage2 = submit(selectNewSiteForm);
+        
+        // Verify form
+        HtmlForm addNewSiteForm = addNewSitePage2.getFormByName("addSiteForm");
+        assertNotNull("Form page is not shown", addNewSiteForm);
+        
+        // Sites number before posting the form.
+        int before = Jenkins.getInstance().getUpdateCenter().getSites().size();
+        
+        // Post form.
+        String id = "newsite1";
+        String url = "http://localhost/update-center.json";
+        addNewSiteForm.getInputByName("_.id").setValueAttribute(id);
+        addNewSiteForm.getInputByName("_.url").setValueAttribute(url);
+        updateSitesPage = submit(addNewSiteForm);
+        
+        // Verify new site is added.
+        assertEquals("No site is added", before + 1, Jenkins.getInstance().getUpdateCenter().getSites().size());
+        UpdateSite site = null;
+        
+        for(UpdateSite test: Jenkins.getInstance().getUpdateCenter().getSites())
+        {
+            if(id.equals(test.getId()))
+            {
+                site = test;
+                break;
+            }
+        }
+        
+        assertNotNull("Added site not found", site);
+        assertEquals("Unexpected site type", TestUpdateSite.class, site.getClass());
+        assertEquals("URL is not configured", url, site.getUrl());
+        
+        // added site is in list.
+        assertNotNull("Added site must be listed", updateSitesPage.getAnchorByHref(((TestUpdateSite)site).getPageUrl()));
+    }
+    
+    public void testDo_addError() throws Exception
+    {
+        // initialize UpdateSites.
+        UpdateSite site1 = new UpdateSite(
+            "test1",
+            "http://example.com/test/update-center.json"
+        );
+        UpdateSite site2 = new ManagedUpdateSite(
+            "test2",
+            "http://example.com/test2/update-center.json",
+            false,
+            null,
+            null,
+            false
+        );
+        Jenkins.getInstance().getUpdateCenter().getSites().clear();
+        Jenkins.getInstance().getUpdateCenter().getSites().add(site1);
+        Jenkins.getInstance().getUpdateCenter().getSites().add(site2);
+        
+        UpdateSitesManager target = new UpdateSitesManager();
+        assertTrue("This test must run with more than one UpdateSite Descriptors registered.", 1 < target.getUpdateSiteDescriptorList().size());
+        
+        WebClient wc = new WebClient();
+        //wc.setPrintContentOnFailingStatusCode(false);
+        
+        // Post without id.
+        {
+            HtmlPage updateSitesPage = wc.goTo(UpdateSitesManager.URL);
+            
+            // follow "Add New Site"
+            HtmlAnchor addNewSiteLink = (HtmlAnchor)updateSitesPage.getElementById("add-new-site-link");
+            HtmlPage addNewSitePage1 = wc.getPage(updateSitesPage.getFullyQualifiedUrl(addNewSiteLink.getHrefAttribute()));
+            
+            // Verify Form
+            HtmlForm selectNewSiteForm = addNewSitePage1.getFormByName("selectNewSiteForm");
+            assertNotNull("A page to select a site type is not shown", selectNewSiteForm);
+            
+            selectNewSiteForm.getInputByValue(Jenkins.getInstance().getDescriptor(SingletonUpdateSite.class).getClass().getName()).setChecked(true);
+            HtmlPage addNewSitePage2 = submit(selectNewSiteForm);
+            
+            // Verify form
+            HtmlForm addNewSiteForm = addNewSitePage2.getFormByName("addSiteForm");
+            assertNotNull("Form page is not shown", addNewSiteForm);
+            
+            // Post without id
+            String url = "http://localhost/update-center.json";
+            
+            addNewSiteForm.getInputByName("_.id").setValueAttribute("");
+            addNewSiteForm.getInputByName("_.url").setValueAttribute(url);
+            try
+            {
+                submit(addNewSiteForm);
+                assertTrue("Must return an exception", false);
+            }
+            catch(Exception e) // TODO: replace with appropriate Exception
+            {
+                System.out.println(e.getClass().getName());
+                System.out.println(e);
+            }
+        }
+        
+        // Post with an duplicated id.
+        {
+            HtmlPage updateSitesPage = wc.goTo(UpdateSitesManager.URL);
+            
+            // follow "Add New Site"
+            HtmlAnchor addNewSiteLink = (HtmlAnchor)updateSitesPage.getElementById("add-new-site-link");
+            HtmlPage addNewSitePage1 = wc.getPage(updateSitesPage.getFullyQualifiedUrl(addNewSiteLink.getHrefAttribute()));
+            
+            // Verify Form
+            HtmlForm selectNewSiteForm = addNewSitePage1.getFormByName("selectNewSiteForm");
+            assertNotNull("A page to select a site type is not shown", selectNewSiteForm);
+            
+            selectNewSiteForm.getInputByValue(Jenkins.getInstance().getDescriptor(SingletonUpdateSite.class).getClass().getName()).setChecked(true);
+            HtmlPage addNewSitePage2 = submit(selectNewSiteForm);
+            
+            // Verify form
+            HtmlForm addNewSiteForm = addNewSitePage2.getFormByName("addSiteForm");
+            assertNotNull("Form page is not shown", addNewSiteForm);
+            
+            // Post without id
+            String url = "http://localhost/update-center.json";
+            addNewSiteForm.getInputByName("_.id").setValueAttribute(site1.getId());
+            addNewSiteForm.getInputByName("_.url").setValueAttribute(url);
+            try
+            {
+                submit(addNewSiteForm);
+                assertTrue("Must return an exception", false);
+            }
+            catch(Exception e) // TODO: replace with appropriate Exception
+            {
+                System.out.println(e.getClass().getName());
+                System.out.println(e);
+            }
+        }
+    }
+    
+    static public class TestUpdateSite extends DescribedUpdateSite
+    {
+        private static final long serialVersionUID = -6892029314071071061L;
+        
+        @DataBoundConstructor
+        public TestUpdateSite(String id, String url)
+        {
+            super(id, url);
+        }
+        
+        @TestExtension("testDo_addWithMultiUpdateSites")
+        static public class DescriptorImpl extends DescribedUpdateSite.Descriptor
+        {
+            @Override
+            public String getDescription()
+            {
+                return "TestUpdateSite";
+            }
+            
+            @Override
+            public String getDisplayName()
+            {
+                return "Provided for testing purpose";
+            }
+        }
+    }
+    
+    static public class SingletonUpdateSite extends DescribedUpdateSite
+    {
+        private static final long serialVersionUID = -6892029314071071061L;
+        
+        private String id;
+        
+        @Override
+        public String getId()
+        {
+            return id;
+        }
+        
+        public void setId(String id)
+        {
+            this.id = id;
+        }
+        
+        private String url;
+        
+        @Override
+        public String getUrl()
+        {
+            return url;
+        }
+        
+        public void setUrl(String url)
+        {
+            this.url = url;
+        }
+        
+        public SingletonUpdateSite(String id, String url)
+        {
+            super(null, null);
+            this.id = id;
+            this.url = url;
+        }
+        
+        static private SingletonUpdateSite INSTANCE = new SingletonUpdateSite(null, null);
+        static public SingletonUpdateSite getInstance()
+        {
+            return INSTANCE;
+        }
+        
+        @TestExtension("testDo_addError")
+        static public class DescriptorImpl extends DescribedUpdateSite.Descriptor
+        {
+            @Override
+            public String getDescription()
+            {
+                return "TestUpdateSite";
+            }
+            
+            @Override
+            public String getDisplayName()
+            {
+                return "Provided for testing purpose";
+            }
+            
+            @Override
+            public DescribedUpdateSite newInstance(StaplerRequest req,
+                    JSONObject formData)
+                    throws hudson.model.Descriptor.FormException
+            {
+                return getInstance();
+            }
+        }
+    }
 }
